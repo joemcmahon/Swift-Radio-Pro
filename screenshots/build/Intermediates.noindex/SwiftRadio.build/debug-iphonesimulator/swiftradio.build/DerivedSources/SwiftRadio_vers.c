79f56dae66fbc6d50dad4f731a6c485959729fa9
Joe McMahon <jmcmahon@ziprecruiter.com>
2019-05-06 09:18:34 -0700

    Don't commit build files

--- screenshots/build/Intermediates.noindex/SwiftRadio.build/debug-iphonesimulator/swiftradio.build/DerivedSources/SwiftRadio_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char SwiftRadioVersionString[];
- extern const double SwiftRadioVersionNumber;
-
- const unsigned char SwiftRadioVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:SwiftRadio  PROJECT:SwiftRadio-1.0" "\n";
- const double SwiftRadioVersionNumber __attribute__ ((used)) = (double)1.0;
--- screenshots/build/Intermediates.noindex/SwiftRadio.build/debug-iphonesimulator/swiftradio.build/objects-normal/x86_64/SwiftRadio_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/joemcmahon/Dropbox/Code/RadioSpiral\ App/screenshots/Build/Intermediates.noindex/SwiftRadio.build/Debug-iphonesimulator/SwiftRadio.build/DerivedSources/SwiftRadio_vers.c
